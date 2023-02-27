c9d2ae2d8fd1b8d697b67eceb517ced31d5b7ce3
Marta Białecka <marta.bialecka@outlook.com>
2023-02-28 00:11:25 +0100

    Coding standard

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     movies = tmdb_client.get_movies(how_many = 8)
-    return render_template("homepage.html", movies=movies)
+    return render_template('homepage.html', movies = movies)
 
 @app.context_processor
 def utility_processor():
     def tmdb_image_url(path, size):
         return tmdb_client.get_poster_url(path, size)
-    return {"tmdb_image_url": tmdb_image_url}
+    return {'tmdb_image_url': tmdb_image_url}
